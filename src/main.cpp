f0019ddaefada9d3c31698e97cf2ccb864979e10
victor.camargo <victor.camargo@rappi.com>
2021-03-02 18:08:05 -0500

    Sensors

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 #include <processors/circular_buffer.h>
 #include <processors/trasnformer.h>
 
+#include <sensors/analog_sensor.h>
 
 CircularBuffer<int> circular = CircularBuffer<int>(10);
+AnalogSensor sensor = AnalogSensor(0x7E, A0);
 
 void setup() {
   Serial.begin(115200);
@@ -13,13 +15,18 @@ void setup() {
 }
 
 void loop() {
-  int raw_read = analogRead(CONFIG::SENSOR_INPUNT);
-  int scaled_read = scaler<int>(raw_read, 1.0);
-  int processed_read = truncate<int>(scaled_read, 1023, 0);
-  circular.append(raw_read);
-  int mean = circular.mean();
   
-  valuePrinter(Serial, mean, "Mean Value");
-  valuePrinter(Serial, processed_read, "Proc Value");
+  sensor.excecute();
+  if(sensor.hasChanged()){
+    int raw_read = sensor.getValue().getValue();
+    int scaled_read = scaler<int>(raw_read, 1.0);
+    int processed_read = truncate<int>(scaled_read, 1023, 0);
+    circular.append(raw_read);
+    int mean = circular.mean();
+
+    valuePrinter(Serial, mean, "Mean Value");
+    valuePrinter(Serial, processed_read, "Proc Value");
+  }
+
   delay(500);
 }
